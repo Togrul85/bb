76ea0c50e294ed568e5d504f4b20405e34e8532d
unknown <toghrul@code.edu.az>
2022-08-16 17:32:53 +0400

    .

---
+++ 5.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork._1
+{
+    class Rectangular : Figure
+    {
+        public Rectangular(float width, float length)
+        {
+            width = Width;
+            length = Length;
+        }
+        private float _width;
+        private float _length;
+        public float Width
+        {
+            get
+            {
+                return _width;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    _width = value;
+                }
+            }
+        }
+        public float Length
+        {
+            get
+            {
+                return _length;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    _length = value;
+                }
+            }
+
+
+        }
+
+        public override float CalcArea()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 7.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork._1
+{
+    class Square:Figure
+    {
+     public Square(float side)
+        {
+            side = Side;
+        }
+        private float _side;
+        public float Side
+        {
+            get
+            {
+                return _side;
+            }
+            set
+            {
+                if (value>=0)
+                {
+                    value = _side;
+                }
+
+
+            }
+        }
+
+        public override float CalcArea()
+        {
+            return Side * Side;
+        }
+    }
+}
